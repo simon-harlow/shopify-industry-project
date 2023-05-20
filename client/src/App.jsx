6218384f450c7a70b6d6a7a3ff0b5554bf69dbe8
Simon Harlow <simonharlow11@gmail.com>
2023-05-20 09:25:27 -0700

    web dev team decided to g with Chakra UI, I have downloaded packages and wrapped app in ChakraProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 import './App.scss';
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             {/* <Route /> */}
           </Routes>
+        <Footer />
     </BrowserRouter>
   );
 }
