ae7edfcc5371a0225a9160e296b28df909dd03a4
Simon Harlow <simonharlow11@gmail.com>
2023-05-21 17:37:38 -0700

    added styling to login page as per design by UX team

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"
-import { Box, Flex, Input, Button, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { AddIcon } from '@chakra-ui/icons'
 import Logo from "../../assets/images/shopify_logo_whitebg.svg"
 
 const Login = () => {
@@ -18,7 +19,7 @@ const Login = () => {
             width="100vw"
             height="100vh"
             zIndex={9999}
-            bg="white"
+            bg="#008060"
             display="flex"
             justifyContent="center"
             alignItems="center"
@@ -26,31 +27,67 @@ const Login = () => {
 			<Flex
 				direction="column"
 				bg="white"
-				p={6}
+				p="2rem"
 				rounded="md"
 				boxShadow="md"
+                justify="space-between"
+                h={456}
+                w={556}
 			>
 				<Box
 					as="img"
 					src={Logo}
 					alt="Shopify Logo"
-					mb={6}
-					w="4rem"
-					h="2rem"
+					w="5rem"
+					h="3rem"
 				/>
+                
+                <Flex direction="column" align="flex-start" justify="center">
+					<Text fontSize="lg" as="b">Choose an account</Text>
+					<Text fontSize="xs" color="grey">to continue to Shopify</Text>
+                </Flex>
 
-				<Input type="text" placeholder="Username" mb={4} />
+				<Flex direction="column" align="flex-start" justify="center">
+					<Flex mb={4} onClick={handleLogin} cursor="pointer" align="center">
+						<Box
+							display="flex"
+							justifyContent="center"
+							alignItems="center"
+							bg="#FFC96B"
+							borderRadius="full"
+							width="2.5rem"
+							height="2.5rem"
+						>
+							<Text color="black" fontSize="xl">
+							XA
+							</Text>
+						</Box>
+						<Flex direction="column" ml={4}>
+							<Text fontSize="md" as="b">Xquenda Andreev</Text>
+							<Text fontSize="xs" color="#828899">xquenda.andreev@gmail.com</Text>
+						</Flex>
+					</Flex>
 
-				<Input type="password" placeholder="Password" mb={4} />
-
-				<Button colorScheme="blue" mb={4} onClick={handleLogin}>
-					Login
-				</Button>
+					<Flex mb={6} align="center">
+					<Box
+						display="flex"
+						justifyContent="center"
+						alignItems="center"
+						bg="#D9D9D9"
+						borderRadius="full"
+						width="2.5rem"
+						height="2.5rem"
+					>
+						<AddIcon color="#828899"/>
+					</Box>
+						<Text ml={4}>Add Account</Text>
+					</Flex>
+                </Flex>
 
 				<Flex justifyContent="flex-end">
-					<Text mr={2}>Help</Text>
-					<Text mr={2}>Privacy</Text>
-					<Text>Terms</Text>
+					<Text fontSize="xs" mr={8}>Help</Text>
+					<Text fontSize="xs" mr={8}>Privacy</Text>
+					<Text fontSize="xs">Terms</Text>
 				</Flex>
 			</Flex>
 		</Box>
